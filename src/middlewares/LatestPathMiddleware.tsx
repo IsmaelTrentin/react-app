31f70d2e337f89d11e6b2927ce517f30cc8b46a8
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-03-22 20:11:53 +0100

    Removed useless user

--- src/middlewares/LatestPathMiddleware.tsx
+++ src/middlewares/LatestPathMiddleware.tsx
@@ -1,18 +1,15 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
 
 const LatestPathMiddleware = () => {
   const location = useLocation();
-  const user = useAuth();
 
   useEffect(() => {
-    if (!user) return;
     const lp = location.pathname;
     const handler = () => localStorage.setItem('latestPath', lp);
     window.addEventListener('beforeunload', handler);
     return () => window.removeEventListener('beforeunload', handler);
-  }, [location.pathname, user]);
+  }, [location.pathname]);
   return null;
 };
 
