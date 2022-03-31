ce57754be6dd13d93d57c31318be0ba6f3d452a0
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-03-31 19:52:06 +0200

    Removed `user` from redux store

--- src/reducers/main.reducer.ts
+++ src/reducers/main.reducer.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import { globalModalReducer } from './GlobalModal/reducer';
-import { userReducer } from './User/reducer';
 
 export const mainReducer = combineReducers({
-  user: userReducer,
   globalModal: globalModalReducer
 });
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,8 @@
 import { createStore } from 'redux';
-import { IUser } from './@types';
 import { IGlobalModalState } from './reducers/GlobalModal/reducer';
 import { mainReducer } from './reducers/main.reducer';
 
 export interface IAppState {
-  user: IUser | null;
   globalModal: IGlobalModalState;
 }
 
