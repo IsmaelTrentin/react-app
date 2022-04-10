92a0eba7eade932e3658b7f5f6b17c6352612cfa
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-04-10 20:40:24 +0200

    Added styling

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,13 +1,18 @@
+import { useMantineTheme } from '@mantine/core';
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import Box from '../../components/Box';
 import Button from '../../components/Button';
 import { useAuth } from '../../hooks/useAuth';
 import { setGlobalModalChildren, showGlobalModal } from '../../reducers/GlobalModal/actions';
+import useStyles from './styles';
 
 interface IProps { }
 
 const Home: React.FC<IProps> = () => {
   const { data: user } = useAuth();
+  const { classes } = useStyles();
+  const theme = useMantineTheme();
   const dispatch = useDispatch();
   const handleShowModal = () => {
     dispatch(
@@ -19,13 +24,22 @@ const Home: React.FC<IProps> = () => {
   };
 
   return (
-    <div>
-      <div>user data: <pre>{JSON.stringify(user, null, 2)}</pre></div>
+    <div className={classes.main}>
       <Button
         onClick={handleShowModal}
       >
         show test modal
       </Button>
+      <Box>
+        <div>user data: </div>
+        <div style={{
+          backgroundColor: theme.other.bgColors.secondaryAlt,
+          padding: '.5rem',
+          borderRadius: 3
+        }}>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </div>
+      </Box>
     </div>
   );
 };
