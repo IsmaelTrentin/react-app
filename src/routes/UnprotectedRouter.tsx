dea0710d6fbae9f3de29ca251c2ac14b41824e9f
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-04-10 20:40:50 +0200

    Moved login logic to the `Login` comp

--- src/routes/UnprotectedRouter.tsx
+++ src/routes/UnprotectedRouter.tsx
@@ -1,24 +1,19 @@
+import Login from './../views/Login';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
-import { SESH_KEY } from '../util/local.storage.keys';
 
 interface IProps { }
 
 const UnprotectedRouter: React.FC<IProps> = () => {
-  const { data: user, refetch } = useAuth();
-  const onLogin = () => {
-    // TODO: authorize and get sesh id
-    localStorage.setItem(SESH_KEY, '1');
-    refetch();
-  };
+  const { data: user } = useAuth();
 
   return (
     <>
       {!user && (
         <Routes>
           {/* Manages all views that DO NOT need auth */}
-          <Route path="*" element={<div onClick={onLogin}>LOGIN PAGE</div>} />
+          <Route path="*" element={<Login />} />
           <Route path="/register" element={<div>REGISTER PAGE</div>} />
         </Routes>
       )}
