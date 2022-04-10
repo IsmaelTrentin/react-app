d0250b5efcc4d95064edc82af778eac3ace0e0f1
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-04-10 20:39:37 +0200

    Created styles for the home page

---
+++ src/views/Home/styles.ts
@@ -0,0 +1,10 @@
+import { createStyles } from '@mantine/core';
+import { view } from './../../util/global.styles';
+
+const styles = createStyles((theme) => ({
+  main: {
+    ...view
+  }
+}));
+
+export default styles;
